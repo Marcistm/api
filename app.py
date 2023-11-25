bd5ad2b4a9f61fb8c926345019e382862113921f
maxuyang <1311757233@qq.com>
2023-11-25 12:38:48 +0800

    删去oa部分,新增sql脚本

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
 import json
-import numpy as np
 import pandas as pd
-import requests
 from flask import Flask, jsonify, request
 from flask_cors import cross_origin, CORS
-
+import redis
 from lib.db import UseMySQL
 from modules.tooling_process.routes import tooling_process
 from utils.common import generate_token, my_md5
 
 app = Flask(__name__)
 app.register_blueprint(tooling_process, url_prefix='/tooling_process')
+redis_client = redis.StrictRedis(host='localhost', port=6379, decode_responses=True)
+print(redis_client)
 random_str = 'hytek20@0_solt~%!$#^&*'  # 加密 盐
 CORS(app, resources={r"/*": {"origins": "*"}})
 pd.set_option('display.max_columns', None)
@@ -32,7 +32,7 @@ def login():
     work_id = request.args.get('work_id')
     passwd = request.args.get('password')
     res_pass = my_md5(passwd, random_str)
-    sql = "select password, has_login, gzmap_privilege,auto_privilege,experiment_privilege,tooling_privilege, name " \
+    sql = "select password, has_login, tooling_privilege, name " \
           "from sys_user " \
           "where work_id = '{}';".format(work_id)
     df = mssql_connect.get_mssql_data(sql)
@@ -43,11 +43,11 @@ def login():
     passwd_db = res['password']
     if res_pass == passwd_db:
         return jsonify(code=200, msg='success', has_login=res['has_login'], token=generate_token(work_id),
-                       gzmap_privilege=res['gzmap_privilege'], auto_privilege=res['auto_privilege'],
-                       tooling_privilege=res['tooling_privilege'], experiment_privilege=res['experiment_privilege'],
+                       tooling_privilege=res['tooling_privilege'],
                        name=res['name'])
     else:
         return jsonify(code=401, msg='密码不正确')
+
 
 @app.route('/change_pswd', methods=['put'])
 @cross_origin(supports_credentials=True)
@@ -67,7 +67,6 @@ def change_passwd():
         return jsonify(code=200, msg=df)
     else:
         return jsonify(code=404, msg="can't find resource")
-
 
 
 if __name__ == '__main__':
