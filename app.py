330a986e369c36a0d60456d7dcc2331223932388
mxy <1311757233@qq.com>
2024-01-05 21:20:58 +0800

    改进

--- app.py
+++ app.py
@@ -4,49 +4,18 @@ from flask import Flask, jsonify, request
 from flask_cors import cross_origin, CORS
 import redis
 from lib.db import UseMySQL
-from modules.tooling_process.routes import tooling_process
+from modules.to_do_list.routes import to_do_list
 from utils.common import generate_token, my_md5
 
 app = Flask(__name__)
-app.register_blueprint(tooling_process, url_prefix='/tooling_process')
+app.register_blueprint(to_do_list, url_prefix='/to_do_List')
 redis_client = redis.StrictRedis(host='localhost', port=6379, decode_responses=True)
 print(redis_client)
-random_str = 'hytek20@0_solt~%!$#^&*'  # 加密 盐
 CORS(app, resources={r"/*": {"origins": "*"}})
 pd.set_option('display.max_columns', None)
 pd.set_option('display.width', None)
 pd.set_option('display.max_rows', None)
 
-
-@app.route('/login', methods=['get'])
-@cross_origin(supports_credentials=True)
-def login():
-    """
-    登录
-    :param: work_id: 工号
-    :param: password: 密码
-    :return: JSON
-    """
-    # wz14142 12345678 2b2786d75fe0463ed7a764532695dae0
-    mssql_connect = UseMySQL()
-    work_id = request.args.get('work_id')
-    passwd = request.args.get('password')
-    res_pass = my_md5(passwd, random_str)
-    sql = "select password, has_login, tooling_privilege, name " \
-          "from sys_user " \
-          "where work_id = '{}';".format(work_id)
-    df = mssql_connect.get_mssql_data(sql)
-    if df.empty:
-        return jsonify(code=404, msg='用户不存在')
-        # return {'code': 404, 'msg': '用户不存在'}
-    res = df.to_dict('records')[0]
-    passwd_db = res['password']
-    if res_pass == passwd_db:
-        return jsonify(code=200, msg='success', has_login=res['has_login'], token=generate_token(work_id),
-                       tooling_privilege=res['tooling_privilege'],
-                       name=res['name'])
-    else:
-        return jsonify(code=401, msg='密码不正确')
 
 
 
--- modules/to_do_list/routes.py
+++ modules/to_do_list/routes.py
@@ -8,26 +8,10 @@ from lib.db import UseMySQL
 import urllib.parse
 from utils.common import find_file
 
-folder_path = 'D:/map'
-tooling_process = Blueprint('tooling_process', __name__)
+to_do_list = Blueprint('to_do_list', __name__)
 
 
-
-
-def file_search(file, type):
-    data = []
-    for file_name in os.listdir(os.path.join(folder_path)):
-        if file in file_name:
-            for item in os.listdir(os.path.join(folder_path, file_name)):
-                if item.endswith('.pdf'):
-                    temp = item.rsplit('.', 1)[0]
-                    data.append(temp)
-    data = list(set(data))
-    df = pd.DataFrame(data, columns=['sub_map'])
-    return df
-
-
-@tooling_process.route('/plan/search', methods=['get'])
+@to_do_list.route('/plan/search', methods=['get'])
 def plan_search():
     work_number = request.args.get('work_number')
     sql = "select process_num,tooling_name,tooling_no,tooling_map,work_order_memo,type,oa_id from work_order " \
@@ -44,9 +28,6 @@ def plan_search():
         type = df.iloc[0]['type']
     else:
         return jsonify(code=404, msg='未找到资源'), 404
-    df2 = file_search(tooling_map, type)
-    df2['process_num'] = df.iloc[0]['process_num']
-    df2 = df2.sort_values(by='sub_map')
     sql = f"select work_row_item,is_print,comp_numbers from work_report where work_number='{work_number}' and number=1"
     df1 = con.get_mssql_data(sql)
     if not df1.empty:
@@ -60,40 +41,7 @@ def plan_search():
     return jsonify(code=200, msg='成功', process_num=str(process_num), tooling_name=tooling_name,
                    comp_numbers=comp_numbers,
                    print_row_item=print_row_item, tooling_map=tooling_map, work_order_memo=work_order_memo,
-                   work_row_item=work_row_item, tooling_no=tooling_no, oa_id=oa_id, data=df2.to_dict('records')), 200
-
-
-def file_process(file):
-    data = []
-    for file_name in os.listdir(os.path.join(folder_path, file)):
-        if file_name.endswith('.pdf'):
-            temp = file_name.rsplit('.', 1)[0]
-            data.append(temp)
-    data = sorted(list(set(data)))
-    df = pd.DataFrame(data, columns=['sub_map'])
-    return df
-
-
-@tooling_process.route('/create', methods=['GET'])
-def create():
-    work_number = request.args.get('work_number')
-    file = request.args.get('file')
-    if work_number:
-        con = UseMySQL()
-        sql = f"select tooling_map,type from work_order where work_number='{work_number}' "
-        df = con.get_mssql_data(sql)
-        if df.empty:
-            return jsonify(code=404, msg='未找到资源'), 404
-        tooling_map = df.iloc[0]['tooling_map']
-        type = df.iloc[0]['type']
-        file = tooling_map
-
-    # 遍历目录中的所有文件和子文件夹
-    if file:
-        df = file_process(file)
-    else:
-        df = pd.DataFrame()
-    return jsonify(code=200, msg='成功', data=df.to_dict('records')), 200  # 返回数据，转换为字典列表格式
+                   work_row_item=work_row_item, tooling_no=tooling_no, oa_id=oa_id, ), 200
 
 
 def generate_work_order_number():
@@ -104,11 +52,13 @@ def generate_work_order_number():
     return year + month + day
 
 
-@tooling_process.route('/create/submit', methods=['post'])
+@to_do_list.route('/create/submit', methods=['post'])
 def tooling_map_submit():
     now = datetime.datetime.now()
     today = now.date()
     val = json.loads(request.get_data())
+    item = val['item']
+    remark = val['remark']
     header = pd.DataFrame(val['header'])
     con = UseMySQL()
     if header.iloc[0]['work_number'] == '':
@@ -130,7 +80,7 @@ def tooling_map_submit():
         return jsonify(code=200, msg='成功', tag=False), 200
 
 
-@tooling_process.route('/del', methods=['GET'])
+@to_do_list.route('/del', methods=['GET'])
 def work_del():
     work_number = request.args.get('work_number')
     sql = f"delete from work_order where work_number='{work_number}'"
@@ -140,8 +90,3 @@ def work_del():
         return jsonify(code=200, msg='删除成功'), 200
     else:
         return jsonify(code=404, msg='删除失败'), 404
-
-
-
-
-
